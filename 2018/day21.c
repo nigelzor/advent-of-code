fe1f48e8bf0f39ce06b71bda50ea032f0fb2cf9f
Neil Gentleman <ngentleman@gmail.com>
2018-12-28 23:56:38 -0800

    day 21, part 2 wip

--- 2018/day21.c
+++ 2018/day21.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int r0 = 3115806, r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0;
-    printf("regs: %i %i %i %i %i %i\n", r0, r1, r2, r3, r4, r5);
+    int r0 = 3115806, /*r1 = 0, r2 = 0, r3 = 0,*/ r4 = 0, r5 = 0;
     r5 = 123;
     while (1) {
         r5 = r5 & 456;
@@ -16,33 +15,27 @@ int main() {
         r4 = r5 | 65536;
         r5 = 13431073;
         while (1) {
-            r3 = r4 & 255;
-            r5 = r5 + r3;
+            r5 = r5 + (r4 & 255);
             r5 = r5 & 16777215;
             r5 = r5 * 65899;
             r5 = r5 & 16777215;
-            r3 = 256 > r4;
-            if (r3) {
+            if (256 > r4) {
                 break;
             }
-            r3 = 0;
+            int r3 = 0;
             while (1) {
-                r2 = r3 + 1;
-                r2 = r2 * 256;
-                r2 = r2 > r4;
-                if (r2) {
+                if (((r3 + 1) * 256) > r4) {
                     r4 = r3;
                     break;
                 }
                 r3 = r3 + 1;
             }
         }
-        r3 = r5 == r0;
-        if (r3) {
+        printf("checking %i == %i\n", r5, r0);
+        if (r5 == r0) {
             break;
         }
     }
-    printf("regs: %i %i %i %i %i %i\n", r0, r1, r2, r3, r4, r5);
     puts("exiting");
     return 0;
 }
