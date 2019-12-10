df274c33e804075282ae6cf5f4088e038aaed758
Neil Gentleman <ngentleman@gmail.com>
2019-12-10 11:03:56 -0800

    clean up day 10
    much cleaner to work in angle alone, instead of unit vectors

--- 2019/day10.py
+++ 2019/day10.py
@@ -4,28 +4,49 @@ import math
 from collections import defaultdict
 
 
+def clockwise_angle(point, other, y_scale=1):
+    """
+    >>> clockwise_angle((0, 0), (0, 1))
+    0.0
+    >>> clockwise_angle((0, 0), (1, 1))
+    45.0
+    >>> clockwise_angle((0, 0), (1, 0))
+    90.0
+    >>> clockwise_angle((0, 0), (1, -1))
+    135.0
+    >>> clockwise_angle((0, 0), (0, -1))
+    180.0
+    >>> clockwise_angle((0, 0), (-1, -1))
+    225.0
+    >>> clockwise_angle((0, 0), (-1, 0))
+    270.0
+    >>> clockwise_angle((0, 0), (-1, 1))
+    315.0
+    """
+    dx = other[0] - point[0]
+    dy = (other[1] - point[1]) * y_scale
+    phase = -cmath.phase(dx + dy * 1j)
+    angle = 90 + math.degrees(phase)
+    if angle < 0:
+        angle += 360
+    return angle
+
+
 def main():
-    space = dict()
+    space = []
     with open('day10_input.txt') as f:
+    # with open('day10_sample4.txt') as f:
         for y, line in enumerate(f):
             for x, v in enumerate(line.strip()):
                 if v == '#':
-                    space[(x, y)] = v
-
-    def angle(point, other):
-        dx = other[0] - point[0]
-        dy = other[1] - point[1]
-        angle = dx + -1j * dy
-        return angle
+                    space.append((x, y))
 
     options = []
     for point in space:
         angles = set()
         for other in space:
             if other != point:
-                a = angle(point, other)
-                k = "{:g}".format(a / abs(a))
-                angles.add(k)
+                angles.add(clockwise_angle(point, other))
         options.append((len(angles), point))
 
     best = max(options)
@@ -35,19 +56,10 @@ def main():
     by_angle = defaultdict(list)
     for other in space:
         if other != point:
-            a = angle(point, other)
-            k = "{:g}".format(a / abs(a))
-            by_angle[k].append(other)
+            a = clockwise_angle(point, other, y_scale=-1)
+            by_angle[a].append(other)
 
-    # clockwise from vertical
-    def clockwise(a):
-        p = -cmath.phase(a)
-        if p < -(math.pi / 2):
-            p += 2 * math.pi
-        return p
-
-    angles = [angle(point, x[0]) for x in by_angle.values()]
-    angles = sorted(angles, key=clockwise)
+    angles = sorted(by_angle.keys())
 
     # closest first
     def distance_from_point(o):
@@ -59,11 +71,10 @@ def main():
     boom = 0
     while sum(len(v) for v in by_angle.values()) > 0:
         for a in angles:
-            k = "{:g}".format(a / abs(a))
-            if by_angle[k]:
+            if by_angle[a]:
                 boom += 1
-                p = by_angle[k].pop()
-                # print('boom', boom, p, k, clockwise(a))
+                p = by_angle[a].pop()
+                # print('boom', boom, p, a)
                 if boom == 200:
                     print(p[0] * 100 + p[1])
                     return
