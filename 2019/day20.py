d86f696f1f3b695c213056e4023972e234abd6e4
Neil Gentleman <ngentleman@gmail.com>
2019-12-31 17:58:00 -0800

    day 20, part 2

--- 2019/day20.py
+++ 2019/day20.py
@@ -12,7 +12,7 @@ DIRECTIONS = {
 }
 
 
-def main(filename):
+def main(filename, recursive=False):
     maze = dict()
     with open(filename) as f:
         for y, line in enumerate(f):
@@ -20,16 +20,25 @@ def main(filename):
             for x, c in enumerate(line.rstrip()):
                 maze[x + y] = c
 
+    xmin = min(k.real for k in maze.keys())
+    xmax = max(k.real for k in maze.keys())
+    ymin = min(k.imag for k in maze.keys())
+    ymax = max(k.imag for k in maze.keys())
+
     def is_feature(v):
         return v.isupper()
 
     def portal_at(n):
         a = maze.get(n, ' ')
         if is_feature(a):
+            outer = n.real <= xmin + 1 or n.real >= xmax - 1 or n.imag <= ymin + 1 or n.imag >= ymax - 1
             for d in DIRECTIONS.values():
                 b = maze.get(n + d, ' ')
                 if is_feature(b):
-                    return ''.join(sorted([a, b]))
+                    suffix = ''
+                    if recursive:
+                        suffix = '-' if outer else '+'
+                    return ''.join(sorted([a, b])) + suffix
         return None
 
     def neighbors(n):
@@ -49,20 +58,37 @@ def main(filename):
         # print(n, r)
         return r
 
+    def for_level(p, l):
+        if p == 'AA-' or p == 'ZZ-':
+            if l == 0:
+                return p[:-1]
+        elif p.endswith('-'):
+            if l > 0:
+                return p[:-1] + str(l)
+        elif p.endswith('+'):
+            return p[:-1] + str(l + 1)
+
     graph = nx.Graph()
     for k, v in maze.items():
         start = portal_at(k)
         if start:
             for o, d in neighbors(k).items():
-                graph.add_edge(start, o, weight=d)
+                if recursive:
+                    for level in range(100):
+                        start_l = for_level(start, level)
+                        end_l = for_level(o, level)
+                        if start_l and end_l:
+                            graph.add_edge(start_l, end_l, weight=d)
+                else:
+                    graph.add_edge(start, o, weight=d)
 
-    print(nx.shortest_path(graph, 'AA', 'ZZ', weight='weight'))
-    print(nx.shortest_path_length(graph, 'AA', 'ZZ', weight='weight') - 1)
+    # print(graph.edges)
+    # pos = nx.spring_layout(graph, iterations=1000)
+    # nx.draw(graph, pos, node_size=0, alpha=0.4, edge_color='r', font_size=16, with_labels=True)
+    # nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, 'weight'))
+    # plt.show()
 
-    pos = nx.spring_layout(graph, iterations=1000)
-    nx.draw(graph, pos, node_size=0, alpha=0.4, edge_color='r', font_size=16, with_labels=True)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, 'weight'))
-    plt.show()
+    print(filename, 'recursive' if recursive else 'simple', nx.shortest_path_length(graph, 'AA', 'ZZ', weight='weight') - 1)
 
 
 if __name__ == "__main__":
@@ -70,3 +96,6 @@ if __name__ == "__main__":
     main('day20_s1.txt')
     main('day20_s2.txt')
     main('day20_input.txt')
+    main('day20_s1.txt', True)
+    main('day20_s3.txt', True)
+    main('day20_input.txt', True)
