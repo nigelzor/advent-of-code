db97c01e46b1f7196a3e9bf3a0a19f3ad910e7e1
Neil Gentleman <ngentleman@gmail.com>
2022-12-04 21:41:36 -0800

    2022: 5 (cleaner)

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,38 +1,40 @@
 import doctest
 
 
-def main():
-    stacks = [list(x) for x in [
-        'ZTFRWJG',
-        'GWM',
-        'JNHG',
-        'JRCNW',
-        'WFSBGQVM',
-        'SRTDVWC',
-        'HBNCDZGV',
-        'SJNMGC',
-        'GPNWCJDL'
-    ]]
+def insert(lists, c, v):
+    while len(lists) <= c:
+        lists.append([])
+    lists[c].insert(0, v)
 
+
+def main():
+    stacks1 = []
+    stacks2 = []
     with open('day5_input.txt') as f:
         for line in f:
-            line = line.strip()
-            if line.startswith('move'):
+            if '[' in line:
+                for i, c in enumerate(line):
+                    if 'A' <= c <= 'Z':
+                        col = i // 4
+                        insert(stacks1, col, c)
+                        insert(stacks2, col, c)
+
+            elif line.startswith('move'):
                 [_, n, _, src, _, dst] = line.split(' ')
                 n = int(n)
                 src = int(src)
                 dst = int(dst)
 
-                # for _ in range(n):
-                #     stacks[dst - 1].append(stacks[src - 1].pop())
+                for _ in range(n):
+                    stacks1[dst - 1].append(stacks1[src - 1].pop())
 
                 move = []
                 for _ in range(n):
-                    move.append(stacks[src - 1].pop())
-                stacks[dst - 1] += reversed(move)
+                    move.append(stacks2[src - 1].pop())
+                stacks2[dst - 1] += reversed(move)
 
-        print(stacks)
-        print(''.join(s[len(s) - 1] for s in stacks))
+        print(''.join(s[len(s) - 1] for s in stacks1))
+        print(''.join(s[len(s) - 1] for s in stacks2))
 
 
 if __name__ == "__main__":
