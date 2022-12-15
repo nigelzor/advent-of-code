e36cfdd993bd82512da7a9f612a68e2516c2970c
Neil Gentleman <ngentleman@gmail.com>
2022-12-14 21:27:44 -0800

    2022: 15.1 (slow)

---
+++ 2022/day15.py
@@ -0,0 +1,54 @@
+import doctest
+import re
+
+
+def print_grid(grid):
+    minx = min(int(c.real) for c in grid.keys())
+    maxx = max(int(c.real) for c in grid.keys())
+    miny = min(int(c.imag) for c in grid.keys())
+    maxy = max(int(c.imag) for c in grid.keys())
+
+    for y in range(miny, maxy + 1):
+        for x in range(minx, maxx + 1):
+            print(grid.get(x + y * 1j, '.'), end='')
+        print('')
+
+
+def diamond(start, distance):
+    for y in range(-distance, distance + 1):
+        for x in range(-(distance - abs(y)), (distance - abs(y)) + 1):
+            yield start + x + y * 1j
+
+
+def main():
+    grid = dict()
+
+    p = re.compile(r'Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)')
+    with open('day15_input.txt') as f:
+        for line in f:
+            match = p.match(line)
+            if match:
+                [sx, sy, bx, by] = [int(v) for v in match.groups()]
+                distance = abs(sx - bx) + abs(sy - by)
+                sensor = sx + sy * 1j
+                beacon = bx + by * 1j
+                for pos in diamond(sensor, distance):
+                    if pos not in grid:
+                        grid[pos] = '#'
+                grid[sensor] = 'S'
+                grid[beacon] = 'B'
+
+    print_grid(grid)
+
+    minx = int(min(p.real for p in grid.keys()))
+    maxx = int(max(p.real for p in grid.keys()))
+
+    y = 2000000
+    print(sum(1 for x in range(minx, maxx + 1) if grid.get(x + y * 1j, '.') == '#'))
+
+
+
+
+if __name__ == "__main__":
+    doctest.testmod()
+    main()
