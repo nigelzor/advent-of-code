4825c95e9f0074feb4dc97a48d130768c78423a6
Neil Gentleman <ngentleman@gmail.com>
2023-12-08 22:19:58 -0800

    2023: 9.2

--- 2023/day9.py
+++ 2023/day9.py
@@ -1,6 +1,5 @@
 import doctest
 import itertools
-import math
 import re
 
 
@@ -12,14 +11,22 @@ def main():
         for line in f:
             lines.append([int(x) for x in whitespace.split(line.strip())])
 
-    part1 = sum(extrapolate(line) for line in lines)
+    part1 = sum(extrapolate_end(line) for line in lines)
     print(f"Part 1: {part1}")
+    part2 = sum(extrapolate_start(line) for line in lines)
+    print(f"Part 2: {part2}")
 
 
-def extrapolate(ns):
+def extrapolate_end(ns):
     if all(n == 0 for n in ns):
         return 0
-    return ns[-1] + extrapolate(list(differences(ns)))
+    return ns[-1] + extrapolate_end(list(differences(ns)))
+
+
+def extrapolate_start(ns):
+    if all(n == 0 for n in ns):
+        return 0
+    return ns[0] - extrapolate_start(list(differences(ns)))
 
 
 def differences(ns):
