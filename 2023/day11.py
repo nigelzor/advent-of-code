299299fde26b70d0e3112a4150585f91e601f54e
Neil Gentleman <ngentleman@gmail.com>
2023-12-10 21:24:32 -0800

    2023: 11.2

--- 2023/day11.py
+++ 2023/day11.py
@@ -39,22 +39,25 @@ def main():
         if all(Point(x, y) not in grid for y in range(max_y + 1)):
             expand_cols.append(x)
 
-    print(max_x, max_y)
-    print(expand_rows, expand_cols)
+    # print(max_x, max_y)
+    # print(expand_rows, expand_cols)
 
-    expanded_grid = dict()
-    for point, value in grid.items():
-        new_x = point.x + sum(1 for c in expand_cols if c < point.x)
-        new_y = point.y + sum(1 for r in expand_rows if r < point.y)
-        expanded_grid[Point(new_x, new_y)] = value
+    def sum_min_distances(expansion):
+        expanded_grid = dict()
+        for point, value in grid.items():
+            new_x = point.x + sum(expansion for c in expand_cols if c < point.x)
+            new_y = point.y + sum(expansion for r in expand_rows if r < point.y)
+            expanded_grid[Point(new_x, new_y)] = value
 
-    part1 = 0
-    for a, b in itertools.combinations(expanded_grid, 2):
-        distance = manhattan(a, b)
-        # print(a, b, distance)
-        part1 += distance
+        result = 0
+        for a, b in itertools.combinations(expanded_grid, 2):
+            distance = manhattan(a, b)
+            # print(a, b, distance)
+            result += distance
+        return result
 
-    print(f"Part 1: {part1}")
+    print(f"Part 1: {sum_min_distances(1)}")
+    print(f"Part 2: {sum_min_distances(1000000 - 1)}")
 
 
 if __name__ == "__main__":
