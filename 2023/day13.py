9636afede3525bc781729a5eb68fa3eadd47b77f
Neil Gentleman <ngentleman@gmail.com>
2023-12-12 21:39:50 -0800

    2023: 13.2

--- 2023/day13.py
+++ 2023/day13.py
@@ -14,28 +14,6 @@ class Point:
         return Point(self.x * other, self.y * other)
 
 
-N = Point(0, -1)
-S = Point(0, 1)
-E = Point(1, 0)
-W = Point(-1, 0)
-
-
-def connects_north(a):
-    return a == "S" or a == "|" or a == "L" or a == "J"
-
-
-def connects_south(a):
-    return a == "S" or a == "|" or a == "7" or a == "F"
-
-
-def connects_west(a):
-    return a == "S" or a == "-" or a == "J" or a == "7"
-
-
-def connects_east(a):
-    return a == "S" or a == "-" or a == "L" or a == "F"
-
-
 def main():
     grids = []
     grid = dict()
@@ -70,46 +48,47 @@ def main():
     grids.append(grid)
 
     part1 = 0
+    part2 = 0
     for i, grid in enumerate(grids):
         max_x = max(c.x for c in grid.keys())
         max_y = max(c.y for c in grid.keys())
-        print(f"Grid {i} {max_x}x{max_y}")
-        print_grid(grid)
+        # print(f"Grid {i} {max_x}x{max_y}")
+        # print_grid(grid)
 
-        def equal_rows(a, b):
+        def count_row_difference(a, b):
+            difference = 0
             for x in range(0, max_x + 1):
                 if grid[Point(x, a)] != grid[Point(x, b)]:
-                    return False
-            return True
+                    difference += 1
+            return difference
 
-        def equal_cols(a, b):
+        def count_col_difference(a, b):
+            difference = 0
             for y in range(0, max_y + 1):
                 if grid[Point(a, y)] != grid[Point(b, y)]:
-                    return False
-            return True
+                    difference += 1
+            return difference
 
-        def is_vertical_reflection(row):
+        def count_vertical_reflection_differences(row):
             # print(f"testing between rows {row} and {row+1}")
             rows_to_check = min(row + 1, max_y - row)
-            for y in range(0, rows_to_check):
-                if not equal_rows(row - y, row + y + 1):
-                    return False
-            return True
+            return sum(count_row_difference(row - y, row + y + 1) for y in range(0, rows_to_check))
 
-        def is_horizontal_reflection(col):
+        def count_horizontal_reflection_differences(col):
             # print(f"testing between cols {col} and {col+1}")
             cols_to_check = min(col + 1, max_x - col)
-            for x in range(0, cols_to_check):
-                if not equal_cols(col - x, col + x + 1):
-                    return False
-            return True
+            return sum(count_col_difference(col - x, col + x + 1) for x in range(0, cols_to_check))
 
-        vertical_reflection = next((row for row in range(0, max_y) if is_vertical_reflection(row)), -1)
-        horizontal_reflection = next((col for col in range(0, max_x) if is_horizontal_reflection(col)), -1)
-        print(f"vr: {vertical_reflection}, hr: {horizontal_reflection}")
-        part1 += 100 * (vertical_reflection + 1) + (horizontal_reflection + 1)
+        vertical_reflection_1 = next((row for row in range(0, max_y) if count_vertical_reflection_differences(row) == 0), -1)
+        horizontal_reflection_1 = next((col for col in range(0, max_x) if count_horizontal_reflection_differences(col) == 0), -1)
+        part1 += 100 * (vertical_reflection_1 + 1) + (horizontal_reflection_1 + 1)
 
-    print(f"Part1: {part1}")
+        vertical_reflection_2 = next((row for row in range(0, max_y) if count_vertical_reflection_differences(row) == 1), -1)
+        horizontal_reflection_2 = next((col for col in range(0, max_x) if count_horizontal_reflection_differences(col) == 1), -1)
+        part2 += 100 * (vertical_reflection_2 + 1) + (horizontal_reflection_2 + 1)
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
 
 
 if __name__ == "__main__":
