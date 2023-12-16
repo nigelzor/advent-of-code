b86cfe81b58ad5ac414c155099942901065b4765
Neil Gentleman <ngentleman@gmail.com>
2023-12-15 23:41:38 -0800

    2023: 16.2

--- 2023/day16.py
+++ 2023/day16.py
@@ -49,54 +49,67 @@ def main():
     max_x = max(c.x for c in grid.keys())
     max_y = max(c.y for c in grid.keys())
 
-    visited = defaultdict(set)
-    pending = []
+    def count_energized(starting_point, starting_direction):
+        visited = defaultdict(set)
+        pending = []
 
-    def visit_next(point, direction):
-        if point.x < 0 or point.x > max_x:
-            return
-        if point.y < 0 or point.y > max_y:
-            return
-        if direction not in visited[point]:
-            visited[point].add(direction)
-            pending.append((point, direction))
+        def visit_next(point, direction):
+            if point.x < 0 or point.x > max_x:
+                return
+            if point.y < 0 or point.y > max_y:
+                return
+            if direction not in visited[point]:
+                visited[point].add(direction)
+                pending.append((point, direction))
 
-    visit_next(Point(0, 0), E)
+        visit_next(starting_point, starting_direction)
 
-    while pending:
-        entering, direction = pending.pop()
-        content = grid.get(entering)
-        if content == '/':
-            if direction == N:
-                direction = E
-            elif direction == E:
-                direction = N
-            elif direction == S:
-                direction = W
-            elif direction == W:
-                direction = S
-        elif content == '\\':
-            if direction == N:
-                direction = W
-            elif direction == E:
-                direction = S
-            elif direction == S:
-                direction = E
-            elif direction == W:
-                direction = N
-        elif content == '-':
-            if direction == N or direction == S:
-                visit_next(entering + W, W)
-                visit_next(entering + E, E)
-                continue
-        elif content == '|':
-            if direction == E or direction == W:
-                visit_next(entering + N, N)
-                visit_next(entering + S, S)
-                continue
-        visit_next(entering + direction, direction)
+        while pending:
+            entering, direction = pending.pop()
+            content = grid.get(entering)
+            if content == '/':
+                if direction == N:
+                    direction = E
+                elif direction == E:
+                    direction = N
+                elif direction == S:
+                    direction = W
+                elif direction == W:
+                    direction = S
+            elif content == '\\':
+                if direction == N:
+                    direction = W
+                elif direction == E:
+                    direction = S
+                elif direction == S:
+                    direction = E
+                elif direction == W:
+                    direction = N
+            elif content == '-':
+                if direction == N or direction == S:
+                    visit_next(entering + W, W)
+                    visit_next(entering + E, E)
+                    continue
+            elif content == '|':
+                if direction == E or direction == W:
+                    visit_next(entering + N, N)
+                    visit_next(entering + S, S)
+                    continue
+            visit_next(entering + direction, direction)
 
-    print(f"Part 1: {len(visited)}")
+        return len(visited)
+
+    print(f"Part 1: {count_energized(Point(0, 0), E)}")
+
+    part2 = 0
+    for y in range(0, max_y + 1):
+        part2 = max(part2, count_energized(Point(0, y), E))
+        part2 = max(part2, count_energized(Point(max_x, y), W))
+    for x in range(0, max_x + 1):
+        part2 = max(part2, count_energized(Point(x, 0), S))
+        part2 = max(part2, count_energized(Point(x, max_y), N))
+
+    print(f"Part 2: {part2}")
 
 
 if __name__ == "__main__":
