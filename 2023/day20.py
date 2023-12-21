4e3ef0a15445e70b29255f3250b644cbf0fa886b
Neil Gentleman <ngentleman@gmail.com>
2023-12-21 02:27:07 -0800

    2023: 20.2
    this was faster to do by eye than by code

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,4 +1,5 @@
 import doctest
+import math
 import re
 from collections import defaultdict
 from dataclasses import dataclass
@@ -68,17 +69,42 @@ def main():
                     send(output, signal, target)
         return low_count, high_count
 
-    total_low_count = 0
-    total_high_count = 0
-    for _ in range(1000):
-        low_count, high_count = press_button()
-        total_low_count += low_count
-        total_high_count += high_count
+    def part1():
+        total_low_count = 0
+        total_high_count = 0
+        for _ in range(1000):
+            low_count, high_count = press_button()
+            total_low_count += low_count
+            total_high_count += high_count
+        return total_low_count * total_high_count
 
-    print(f'Part 1: {total_low_count * total_high_count}')
+    print(f'Part 1: {part1()}')
 
+    def part2():
+        def preceding(module_name):
+            return [k for k, v in modules.items() if module_name in v.outputs]
 
+        def bits_to_int(bits):
+            value = 0
+            for bit in reversed(bits):
+                value *= 2
+                if bit:
+                    value |= 1
+            return value
 
+        def counter_period(counter):
+            bits = []
+            bit = next(m for m in preceding(counter) if m in modules['broadcaster'].outputs)
+            while bit:
+                bits.append(counter in modules[bit].outputs)
+                bit = next((m for m in modules[bit].outputs if m != counter), None)
+            return bits_to_int(bits)
+
+        counters = [preceding(x)[0] for x in preceding(preceding('rx')[0])]
+        periods = [counter_period(c) for c in counters]
+        return math.lcm(*periods)
+
+    print(f'Part 2: {part2()}')
 
 
 if __name__ == "__main__":
