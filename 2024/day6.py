f8f80de11ef12b1066fb9a4cfe539e8a4ff3209e
Neil Gentleman <ngentleman@gmail.com>
2024-12-05 22:36:37 -0800

    2024: 6.2

--- 2024/day6.py
+++ 2024/day6.py
@@ -19,6 +19,23 @@ W = Point(-1, 0)
 turn_right = {N: E, E: S, S: W, W: N}
 
 
+def simulate(map, position, direction):
+    visited = set()
+    try:
+        while True:
+            if map[position + direction] == "#":
+                direction = turn_right[direction]
+            else:
+                position += direction
+
+            if (position, direction) in visited:
+                return "loop"
+
+            visited.add((position, direction))
+    except KeyError:  # walked out of bounds
+        return {v[0] for v in visited}
+
+
 def main():
     map = dict()
 
@@ -31,19 +48,16 @@ def main():
 
     guard_direction = N
 
-    visited = set()
-    try:
-        while True:
-            if map[guard_position + guard_direction] == "#":
-                guard_direction = turn_right[guard_direction]
-            else:
-                guard_position += guard_direction
+    visited = simulate(map, guard_position, guard_direction)
+    part1 = len(visited)
+    print(f"part 1: {part1}")
 
-            visited.add(guard_position)
-    except KeyError:
-        pass  # walked out of bounds
-
-    print(f"part 1: {len(visited)}")
+    part2 = 0
+    for possible_obstacle in visited:
+        new_map = {**map, possible_obstacle: "#"}
+        if simulate(new_map, guard_position, guard_direction) == "loop":
+            part2 += 1
+    print(f"part 2: {part2}")
 
 
 if __name__ == "__main__":
