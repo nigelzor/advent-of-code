b205717307cbc9584cbda184bdbc25aa7e0503c7
Neil Gentleman <ngentleman@gmail.com>
2024-12-14 22:41:52 -0800

    2024: 15.2

--- 2024/day15.py
+++ 2024/day15.py
@@ -1,5 +1,6 @@
 import doctest
 from dataclasses import dataclass
+from operator import attrgetter
 
 
 @dataclass(frozen=True, order=True)
@@ -9,6 +10,9 @@ class Point:
 
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other):
+        return Point(self.x * other.x, self.y * other.y)
 
 
 N = Point(0, -1)
@@ -24,9 +28,77 @@ DIRECTIONS = {
 }
 
 
+def part1(warehouse, position, instructions):
+    def try_move(p, d):
+        into = warehouse.get(p + d)
+        if into == "#":
+            return False
+        if (into is None) or (into == "O" and try_move(p + d, d)):
+            if p in warehouse:
+                warehouse[p + d] = warehouse[p]
+                del warehouse[p]
+            return True
+        return False
+
+    for n, i in enumerate(instructions, 1):
+        direction = DIRECTIONS[i]
+        if try_move(position, direction):
+            position += direction
+
+    return sum(k.y * 100 + k.x for k, v in warehouse.items() if v == "O")
+
+
+def part2(warehouse, position, instructions):
+    def collect_move(p, d, pending):
+        into = warehouse.get(p + d)
+        if into == "#":
+            return False
+        if into is None:
+            pending.add(p)
+            return True
+        if into == "[":
+            if collect_move(p + d, d, pending) and (
+                d in (E, W) or collect_move(p + d + E, d, pending)
+            ):
+                pending.add(p)
+                return True
+        if into == "]":
+            if collect_move(p + d, d, pending) and (
+                d in (E, W) or collect_move(p + d + W, d, pending)
+            ):
+                pending.add(p)
+                return True
+        return False
+
+    for n, i in enumerate(instructions, 1):
+        direction = DIRECTIONS[i]
+        to_move = set()
+        if collect_move(position, direction, to_move):
+            sorted_to_move = sorted(
+                to_move,
+                key=attrgetter("x" if direction in (E, W) else "y"),
+                reverse=direction in (S, E),
+            )
+            for p in sorted_to_move:
+                warehouse[p + direction] = warehouse[p]
+                del warehouse[p]
+            position += direction
+
+    return sum(k.y * 100 + k.x for k, v in warehouse.items() if v == "[")
+
+
+def print_warehouse(warehouse):
+    height = max(k.y for k in warehouse)
+    width = max(k.x for k in warehouse)
+    for y in range(height + 1):
+        for x in range(width + 1):
+            print(warehouse.get(Point(x, y), " "), end="")
+        print()
+
+
 def main():
-    position = Point(0, 0)
     warehouse = {}
+    position = Point(0, 0)
     instructions = ""
 
     with open("day15_input.txt") as f:
@@ -45,31 +117,21 @@ def main():
         for line in lines:
             instructions += line.strip()
 
-    def try_move(p, d):
-        into = warehouse.get(p + d)
-        if into == "#":
-            return False
-        if (into is None) or (into == "O" and try_move(p + d, d)):
-            if p in warehouse:
-                warehouse[p + d] = warehouse[p]
-                del warehouse[p]
-            return True
-        return False
+    scale = Point(2, 1)
+    wide_warehouse = {}
+    for k, v in warehouse.items():
+        if v == "#":
+            wide_warehouse[k * scale] = "#"
+            wide_warehouse[k * scale + E] = "#"
+        elif v == "O":
+            wide_warehouse[k * scale] = "["
+            wide_warehouse[k * scale + E] = "]"
+        elif v == "@":
+            wide_warehouse[k * scale] = "@"
+    wide_position = position * scale
 
-    def gps_total():
-        total = 0
-        for k, v in warehouse.items():
-            if v == "O":
-                total += k.y * 100 + k.x
-        return total
-
-    for n, i in enumerate(instructions, 1):
-        direction = DIRECTIONS[i]
-        if try_move(position, direction):
-            position += direction
-
-    print(f"part 1: {gps_total()}")
-    # print(f"part 2: {part2}")
+    print(f"part 1: {part1(warehouse, position, instructions)}")
+    print(f"part 2: {part2(wide_warehouse, wide_position, instructions)}")
 
 
 if __name__ == "__main__":
