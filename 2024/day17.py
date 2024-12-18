7533cf219957e94aa3ef3bd34514ced4a0e66ea3
Neil Gentleman <ngentleman@gmail.com>
2024-12-18 00:41:42 -0800

    2024: 17.2

--- 2024/day17.py
+++ 2024/day17.py
@@ -78,19 +78,86 @@ def run_for_output(registers, instructions):
     return ",".join((str(c) for c in computer.output))
 
 
-def main():
-    registers = []
+def to_c(registers, instructions):
+    print(f"""
+int main() {{
+    int a = {registers[0]};
+    int b = {registers[1]};
+    int c = {registers[2]};
+    """)
 
+    def combo_value(operand):
+        if 0 <= operand <= 3:
+            return operand
+        if 4 <= operand <= 6:
+            return "abc"[operand - 4]
+        raise ValueError(f"invalid operand {operand}")
+
+    for i in range(0, len(instructions), 2):
+        opcode = instructions[i]
+        operand = instructions[i + 1]
+        print(f"label_{i}:")
+        if opcode == 0:  # adv
+            print(f"\ta = a / pow(2, {combo_value(operand)});")
+        elif opcode == 1:  # bxl
+            print(f"\tb = b ^ {operand};")
+        elif opcode == 2:  # bst
+            print(f"\tb = {combo_value(operand)} % 8;")
+        elif opcode == 3:  # jnz
+            print("    if (a != 0) {\n" f"        goto label_{operand};\n" "    }")
+        elif opcode == 4:  # bxc (operand ignored)
+            print("\tb = b ^ c;")
+        elif opcode == 5:  # out
+            print(f"\toutput({combo_value(operand)} % 8);")
+        elif opcode == 6:  # bdv
+            print(f"\tb = a / pow(2, {combo_value(operand)});")
+        elif opcode == 7:  # cdv
+            print(f"\tc = a / pow(2, {combo_value(operand)});")
+
+    print("}")
+
+
+def reconstructed(a):
+    """
+    >>> list(reconstructed(63281501))
+    [3, 4, 3, 1, 7, 6, 5, 6, 0]
+    """
+    while a:
+        b = a % 8
+        b = b ^ 5
+        c = a // pow(2, b)
+        b = b ^ c
+        a = a // 8
+        b = b ^ 6
+        yield b % 8
+
+
+def part2(instructions):
+    candidates = {0}
+    for i in range(len(instructions)):
+        to_match = instructions[-i - 1 :]
+        candidates = {
+            (h << 3) | t
+            for h in candidates
+            for t in range(8)
+            if list(reconstructed((h << 3) | t)) == to_match
+        }
+    return min(candidates)
+
+
+def main():
     with open("day17_input.txt") as f:
         lines = iter(line.strip() for line in f)
-
-        registers.append(int(next(lines).split(":")[1]))
-        registers.append(int(next(lines).split(":")[1]))
-        registers.append(int(next(lines).split(":")[1]))
+        registers = [
+            int(next(lines).split(":")[1]),
+            int(next(lines).split(":")[1]),
+            int(next(lines).split(":")[1]),
+        ]
         next(lines)
         instructions = [int(c) for c in next(lines).split(":")[1].split(",")]
 
     print(f"part 1: {run_for_output(registers, instructions)}")
+    print(f"part 2: {part2(instructions)}")
 
 
 if __name__ == "__main__":
