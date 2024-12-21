c4cb96d9e095cdcb1bc4995bd30b2c3baa67aafc
Neil Gentleman <ngentleman@gmail.com>
2024-12-21 00:24:05 -0800

    2024: 21.2

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,19 +1,18 @@
 import doctest
+from functools import lru_cache
 
 
-def bi_map(*lines: str):
+def build_pad_map(*lines: str):
     mapping = dict()
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
-            p = (x, y)
-            mapping[p] = c
-            mapping[c] = p
+            mapping[c] = (x, y)
     return mapping
 
 
-door_pad = bi_map("789", "456", "123", " 0A")
+door_pad = build_pad_map("789", "456", "123", " 0A")
 
-control_pad = bi_map(" ^A", "<v>")
+control_pad = build_pad_map(" ^A", "<v>")
 
 
 def to_movement(src, dst):
@@ -31,7 +30,7 @@ def to_movement(src, dst):
     return presses
 
 
-def movement_options(src, dst):
+def door_movement_options(src, dst):
     m = to_movement(src, dst)
     if src[0] == 0 and dst[1] == 3:
         return {m + "A"}  # must move x first
@@ -42,40 +41,49 @@ def movement_options(src, dst):
     return {m + "A", m[::-1] + "A"}
 
 
+def control_movement_options(src, dst):
+    m = to_movement(src, dst)
+    if src[0] == 0 and dst[1] == 0:
+        return {m + "A"}  # must move x first
+    if src[1] == 0 and dst[0] == 0:
+        return {m[::-1] + "A"}  # must move y first
+
+    # two permutations: y before x and x before y
+    return {m + "A", m[::-1] + "A"}
+
+
 def optimal_input(code, levels):
     """
     >>> optimal_input("029A", 0)
-    '<A^A>^^AvvvA'
+    12
     >>> optimal_input("029A", 1)
-    'v<<A>>^A<A>AvA<^AA>A<vAAA>^A'
+    28
     >>> optimal_input("029A", 2)
-    '<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A'
+    68
     """
-    to_press = ""
+    to_press = 0
     position = door_pad["A"]
     for c in code:
         target = door_pad[c]
         to_press += min(
-            (expand_recursively(o, levels) for o in movement_options(position, target)),
-            key=len,
+            expand_recursively(o, levels)
+            for o in door_movement_options(position, target)
         )
         position = target
     return to_press
 
 
+@lru_cache(2048)
 def expand_recursively(code, levels):
     if levels == 0:
-        return code
-    to_press = ""
+        return len(code)
+    to_press = 0
     position = control_pad["A"]
     for c in code:
         target = control_pad[c]
         to_press += min(
-            (
-                expand_recursively(o, levels - 1)
-                for o in movement_options(position, target)
-            ),
-            key=len,
+            expand_recursively(o, levels - 1)
+            for o in control_movement_options(position, target)
         )
         position = target
     return to_press
@@ -86,10 +94,10 @@ def main():
         codes = [line.strip() for line in f]
 
     def complexity(code, levels):
-        optimal = optimal_input(code, levels)
-        return int(code[:3]) * len(optimal)
+        return int(code[:3]) * optimal_input(code, levels)
 
     print(f"part 1: {sum(complexity(code, 2) for code in codes)}")
+    print(f"part 2: {sum(complexity(code, 25) for code in codes)}")
 
 
 if __name__ == "__main__":
