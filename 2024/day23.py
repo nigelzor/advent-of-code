cd2550148e83d7d0eb49efbb9678279760a2c947
Neil Gentleman <ngentleman@gmail.com>
2024-12-22 22:33:15 -0800

    2024: 23.2

--- 2024/day23.py
+++ 2024/day23.py
@@ -12,7 +12,11 @@ def main():
     triples = list(c for c in nx.enumerate_all_cliques(g) if len(c) == 3)
     part1 = sum(1 for t in triples if any(c.startswith("t") for c in t))
 
+    largest, _ = nx.max_weight_clique(g, weight=None)
+    part2 = ",".join(sorted(largest))
+
     print(f"part 1: {part1}")
+    print(f"part 2: {part2}")
 
 
 if __name__ == "__main__":
